c329db56bd414ce3a2f180c9909049c1570862c4
Robin Butler <Robin@Robins-MacBook-Air.local>
2017-08-09 10:09:07 -0400

    daily project

---
+++ main.js
@@ -0,0 +1,39 @@
+/get Robins Log Info
+
+fetch('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+  //Results
+      response.json().then(function(data) {
+
+          let title = data.results.title;
+          console.log(`Title: ${title}`);
+          let href = data.results.href ;
+          console.log (`href: ${href}`)
+          let ingredients = data.results.ingredients;
+          console.log (`ingredients: ${ingredients}`)
+          let thumbnail = data.results.thumbnail ;
+          console.log (`thumbnail: ${thumbnail}`)
+
+          let searchcontent= 
+
+
+
+          let markup = `
+                     <div class="title">
+                       <p>Title: ${title}</p>
+                       <p>Episode ID: ${data.results[0].episode_id}</p>
+                       <ul>Characters:
+                         <li>${data.results[0].characters}</li>
+                       </ul>
+                       ${renderCharacters(data.results.characters)}
+                     </div>
+                   `
+
+                   document.body.innerHTML = markup;
